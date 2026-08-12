5d7b466fc187d0095d9ea6bbc246e763cda0f84b
Ismaelmene <agrosafra7@gmail.com>
2026-08-12 16:05:31 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Substitua pelos dados do SEU projeto Firebase (Configurações do projeto > Seus apps > SDK setup)
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ_gtgDDXgJnZbVicQSveyyBWtE4xClKM",
+  authDomain: "aimexultra.firebaseapp.com",
+  projectId: "aimexultra",
+  storageBucket: "aimexultra.firebasestorage.app",
+  messagingSenderId: "1012765024635",
+  appId: "1:1012765024635:web:a21b2ebf611352496b5dde"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
